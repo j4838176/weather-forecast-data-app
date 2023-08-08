7b02cdedbe782bd3ca76b8d797710a674b111605
j4838176 <j4838176@gmail.com>
2023-08-08 16:52:01 +0300

    Final commit

--- main.py
+++ main.py
@@ -5,11 +5,29 @@ from backend import get_data
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select yhe number of forecast days")
+                 help="Select the number of forecast days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(place, days, option)
+if place:
+    # Get the temperature plot
+    try:
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png",
+                     "Clouds": "images/cloud.png",
+                     "Rain": "images/rain.png",
+                     "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.write("That place does not exist.")
