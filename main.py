61a5705acd5dc6784f256f4dc9157e0741b188b5
j4838176 <j4838176@gmail.com>
2023-08-08 16:24:53 +0300

    Plot real data dynamically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -8,14 +9,7 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+d, t = get_data(place, days, option)
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperature = [10, 11, 15]
-    temperature = [days * i for i in temperature]
-    return dates, temperature
-
-
-d, t = get_data(days)
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
